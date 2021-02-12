fe16b2b08e3b2527bc3069f2cd3d0061144a9412
Pingumask <pingumask@elanta.fr>
2021-02-12 11:14:50 +0100

    Under construction

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-    <title>Under Construction</title>
+    <title>Fresh PiVee : Under Construction</title>
 </head>
 <body>
     <h1>Under Construction</h1>
