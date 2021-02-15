a4ad117910a56f1de4160f7d5e0182ae215084ea
Pingumask <Pingumask@elanta.fr>
2021-02-15 10:41:28 +0100

    ajout du sql d'initialisation du projet

---
+++ sql/FreshPiVee_mysql_create.sql
@@ -0,0 +1,58 @@
+CREATE TABLE `user` (
+	`id_user` INT NOT NULL AUTO_INCREMENT,
+	`nickname` TEXT NOT NULL UNIQUE,
+	`email` TEXT NOT NULL UNIQUE,
+	`password` TEXT NOT NULL,
+	`birthday` DATE NOT NULL,
+	`signed_up` DATETIME NOT NULL,
+	PRIMARY KEY (`id_user`)
+);
+
+CREATE TABLE `follow` (
+	`id_follow` INT NOT NULL AUTO_INCREMENT,
+	`id_follower` INT NOT NULL,
+	`id_followed` INT NOT NULL,
+	PRIMARY KEY (`id_follow`)
+);
+
+CREATE TABLE `upload` (
+	`id_upload` INT NOT NULL AUTO_INCREMENT,
+	`uploader` INT NOT NULL,
+	`upload_time` DATETIME NOT NULL,
+	`title` TEXT NOT NULL,
+	`description` TEXT NOT NULL,
+	`path` TEXT NOT NULL,
+	`media_type` ENUM('picture', 'video') NOT NULL,
+	PRIMARY KEY (`id_upload`)
+);
+
+CREATE TABLE `evaluation` (
+	`id_evaluation` INT NOT NULL AUTO_INCREMENT,
+	`id_user` INT NOT NULL,
+	`id_upload` INT NOT NULL,
+	`evaluation_type` ENUM('dislike', 'like', 'favorite') NOT NULL,
+	PRIMARY KEY (`id_evaluation`)
+);
+
+CREATE TABLE `comment` (
+	`id_comment` INT NOT NULL AUTO_INCREMENT,
+	`id_user` INT NOT NULL,
+	`id_upload` INT NOT NULL,
+	`comment_time` DATETIME NOT NULL,
+	PRIMARY KEY (`id_comment`)
+);
+
+ALTER TABLE `follow` ADD CONSTRAINT `follower` FOREIGN KEY (`id_follower`) REFERENCES `user`(`id_user`);
+
+ALTER TABLE `follow` ADD CONSTRAINT `followed` FOREIGN KEY (`id_followed`) REFERENCES `user`(`id_user`);
+
+ALTER TABLE `upload` ADD CONSTRAINT `uploader` FOREIGN KEY (`uploader`) REFERENCES `user`(`id_user`);
+
+ALTER TABLE `evaluation` ADD CONSTRAINT `evaluater` FOREIGN KEY (`id_user`) REFERENCES `user`(`id_user`);
+
+ALTER TABLE `evaluation` ADD CONSTRAINT `evaluated` FOREIGN KEY (`id_upload`) REFERENCES `upload`(`id_upload`);
+
+ALTER TABLE `comment` ADD CONSTRAINT `commenter` FOREIGN KEY (`id_user`) REFERENCES `user`(`id_user`);
+
+ALTER TABLE `comment` ADD CONSTRAINT `commented` FOREIGN KEY (`id_upload`) REFERENCES `upload`(`id_upload`);
+
