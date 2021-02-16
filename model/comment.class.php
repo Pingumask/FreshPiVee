aa8a91498a95674c92dcf268899bb2e5ddc246b2
jojo19-github <73101593+jojo19-github@users.noreply.github.com>
2021-02-16 11:11:23 +0000

    class php

---
+++ model/comment.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class Comment{
+    public $id_comment;
+	public $user;
+	public $upload;
+	public $comment_time;
+    public $comment_content;
+}
---
+++ model/evaluation.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class Evaluation{
+    public $id_evaluation;
+	public $user;
+	public $upload;
+	public $evaluation_type;
+	public $evaluation_time;
+}
