18288b1bce1c231025514557896c372ebb655d76
Pingumask <pingumask@elanta.fr>
2021-02-16 16:29:12 +0100

    Finition, template

--- index.php
+++ index.php
@@ -4,34 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./documentation/style/style.css">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Fresh PiVee</title>
 </head>
 <body>
-    
     <!--============================= MENU HAUT =========================================-->
-        <header>
-            <ul class="buttons_haut">
-                    <li><a href=""><img src="documentation\LogoFresPiVee.psd" alt="logo Fresh PiVee"></a></li>
-                    <li><a href=""><h1 id="titre">Fresh</br>PiVee</h1></a></li>
-                    <li><a href=""><img src="ressources\icone_haut\upload.svg" alt="icone Uploade vidéo ou images"></a></li>
-                    <li><a href=""><img src="ressources\icone_haut\chercher.svg" alt="icone de recherches"></a></li>
-                    <li><a href=""><img src="ressources\icone_haut\connexion_inscription.svg" alt="icones Conexions ou inscriptions"></a></li>
-            </ul>
+        <header id="menu_haut" class="menu">
+            <a href="" class="logo">
+                <img src="./ressources/image/LogoFresPiVee.svg" alt="logo Fresh PiVee">
+                <h1>
+                    <span class="fresh">Fresh</span>
+                    <br/>
+                    <span class="pivee">PiVee</span>
+                </h1>
+            </a>
+            <input type="text" name="search" placeholder="search"/>
+            <a href=""><img src="./ressources/image/icone_header/chercher.svg" alt="icone de recherches" id="search_button"></a>
+            <a href=""><img src="./ressources/image/icone_header/upload.svg" alt="icone Uploade vidéo ou images"></a>
+            <a href=""><img src="./ressources/image/icone_header/avatar.svg" alt="icones Conexions ou inscriptions"></a>
         </header>
     <!--========================= CONTENU  ==============================================-->
         <main>
             
         </main>
     <!--========================= navigation ============================================-->
-        <nav class="menu_bas">
-            <ul class="buttons_bas">
-                <li><a href=""><img src="ressources\icone_haut\icone_image.svg" alt="mes images"></a></li>
-                <li><a href=""><img src="ressources\icone_haut\icone_video.svg" alt="Uploade de vidéo ou images"></a></li>
-                <li><a href=""><img src="ressources\icone_bas\.svg" alt="icone d'accueil"></a></li>
-            	<li><a href=""><img src="ressources\icone_bas\icone_amis.svg" alt="Conexions ou inscriptions"></a></li>
-                <li><a href=""><img src="ressources\icone_bas\icone_settings.svg" alt="réglages"></a></li>
-            </ul>
+        <nav id="menu_bas" class="menu">
+            <a href="" class="logo"><img src="./ressources/image/LogoFresPiVee.svg" alt="logo Fresh PiVee"><h1><span class="fresh">Fresh</span><br/><span class="pivee">PiVee</span></h1></a>
+            <a href=""><img src="./ressources/image/icone_nav/icone_picture.svg" alt="mes images"><span>Images</span></a>
+            <a href=""><img src="./ressources/image/icone_nav/icone_video.svg" alt="Uploade de vidéo ou images"><span>Vidéos</span></a>
+            <a href="" id="home_button" class="active_menu"><img src="./ressources/image/icone_nav/icone_accueil.svg" alt="icone d'accueil"><span>Tendances</span></a>
+            <a href=""><img src="./ressources/image/icone_nav/icone_amis.svg" alt="Amis"><span>Amis</span></a>
+            <a href=""><img src="./ressources/image/icone_nav/icone_settings.svg" alt="réglages"><span>Préférences</span></a>
         </nav>
 </body>
 </html>
