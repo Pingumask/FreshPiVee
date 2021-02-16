3f4b8022b9862d607d2ffe219d49679abb60ef16
stein13-web <73102749+stein13-web@users.noreply.github.com>
2021-02-16 11:28:07 +0100

    creation template generale

--- index.php
+++ index.php
@@ -4,10 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
-    <title>Fresh PiVee : Under Construction</title>
+    <link rel="stylesheet" href="./documentation/style/style.css">
+    <title>Fresh PiVee</title>
 </head>
 <body>
-    <h1>Under Construction</h1>
+    
+    <!--============================= MENU HAUT =========================================-->
+        <header>
+            <ul class="buttons_haut">
+                    <li><a href=""><img src="documentation\LogoFresPiVee.psd" alt="logo Fresh PiVee"></a></li>
+                    <li><a href=""><h1 id="titre">Fresh</br>PiVee</h1></a></li>
+                    <li><a href=""><img src="ressources\icone_haut\upload.svg" alt="icone Uploade vidéo ou images"></a></li>
+                    <li><a href=""><img src="ressources\icone_haut\chercher.svg" alt="icone de recherches"></a></li>
+                    <li><a href=""><img src="ressources\icone_haut\connexion_inscription.svg" alt="icones Conexions ou inscriptions"></a></li>
+            </ul>
+        </header>
+    <!--========================= CONTENU  ==============================================-->
+        <main>
+            
+        </main>
+    <!--========================= navigation ============================================-->
+        <nav class="menu_bas">
+            <ul class="buttons_bas">
+                <li><a href=""><img src="ressources\icone_haut\icone_image.svg" alt="mes images"></a></li>
+                <li><a href=""><img src="ressources\icone_haut\icone_video.svg" alt="Uploade de vidéo ou images"></a></li>
+                <li><a href=""><img src="ressources\icone_bas\.svg" alt="icone d'accueil"></a></li>
+            	<li><a href=""><img src="ressources\icone_bas\icone_amis.svg" alt="Conexions ou inscriptions"></a></li>
+                <li><a href=""><img src="ressources\icone_bas\icone_settings.svg" alt="réglages"></a></li>
+            </ul>
+        </nav>
 </body>
 </html>
