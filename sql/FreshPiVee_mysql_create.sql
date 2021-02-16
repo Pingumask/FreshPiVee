4e4e769ecbcf413bb76b3cd8cb57cc5224ccd5f1
Pingumask <pingumask@elanta.fr>
2021-02-16 10:52:56 +0100

    Correction de la bdd

--- sql/FreshPiVee_mysql_create.sql
+++ sql/FreshPiVee_mysql_create.sql
@@ -1,19 +1,19 @@
 CREATE TABLE `user` (
 	`id_user` INT NOT NULL AUTO_INCREMENT,
-	`nickname` TEXT NOT NULL UNIQUE,
-	`email` TEXT NOT NULL UNIQUE,
+	`nickname` TEXT NOT NULL ,
+	`email` TEXT NOT NULL ,
 	`password` TEXT NOT NULL,
 	`birthday` DATE NOT NULL,
 	`signed_up` DATETIME NOT NULL,
 	PRIMARY KEY (`id_user`)
-);
+) ENGINE=INNODB CHARACTER SET utf8;
 
 CREATE TABLE `follow` (
 	`id_follow` INT NOT NULL AUTO_INCREMENT,
 	`id_follower` INT NOT NULL,
 	`id_followed` INT NOT NULL,
 	PRIMARY KEY (`id_follow`)
-);
+) ENGINE=INNODB CHARACTER SET utf8;
 
 CREATE TABLE `upload` (
 	`id_upload` INT NOT NULL AUTO_INCREMENT,
@@ -24,7 +24,7 @@ CREATE TABLE `upload` (
 	`path` TEXT NOT NULL,
 	`media_type` ENUM('picture', 'video') NOT NULL,
 	PRIMARY KEY (`id_upload`)
-);
+) ENGINE=INNODB CHARACTER SET utf8;
 
 CREATE TABLE `evaluation` (
 	`id_evaluation` INT NOT NULL AUTO_INCREMENT,
@@ -33,7 +33,7 @@ CREATE TABLE `evaluation` (
 	`evaluation_type` ENUM('dislike', 'like', 'favorite') NOT NULL,
 	`evaluation_time` DATETIME NOT NULL,
 	PRIMARY KEY (`id_evaluation`)
-);
+) ENGINE=INNODB CHARACTER SET utf8;
 
 CREATE TABLE `comment` (
 	`id_comment` INT NOT NULL AUTO_INCREMENT,
@@ -41,7 +41,7 @@ CREATE TABLE `comment` (
 	`id_upload` INT NOT NULL,
 	`comment_time` DATETIME NOT NULL,
 	PRIMARY KEY (`id_comment`)
-);
+) ENGINE=INNODB CHARACTER SET utf8;
 
 ALTER TABLE `follow` ADD CONSTRAINT `follower` FOREIGN KEY (`id_follower`) REFERENCES `user`(`id_user`);
 
