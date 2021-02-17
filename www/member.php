04fb61ffcb9f517d12674aba0a2b76e968928096
Pingumask <Pingumask@elanta.fr>
2021-02-17 20:25:31 +0100

    Correction du controleur de la page membre pour tenir compte des modifications sur la classe user

--- www/member.php
+++ www/member.php
@@ -3,7 +3,6 @@ require_once('./model/user.class.php');
 
 $currentPage="member";
 
-$member = new User();
-$member->getUserFromDb(1);
+$member = User::loadById(1);
 
 require_once("./view/template.php");
