a9c6be0d26b89a68321f687f55d706090e33d7af
Pingumask <Pingumask@elanta.fr>
2021-02-24 15:24:43 +0100

    Un peu de css

--- www/view/components/comment_section.php
+++ www/view/components/comment_section.php
@@ -1,7 +1,7 @@
 <section id="comments">
     <form action="handle_new_comment.php" method="post">
         <input type="hidden" name="id_upload" value="<?php echo $media->id_upload?>" />
-        <textarea name="comment_content" id="" cols="30" rows="10"></textarea>
+        <textarea name="comment_content" id="comment_content" cols="30" rows="10"></textarea>
         <input type="submit" value="📩"/>
     </form>
     <?php
