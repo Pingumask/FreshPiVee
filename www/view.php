ee56b348032fed944822c0da4561adabba089c78
Pingumask <Pingumask@elanta.fr>
2021-02-24 10:51:59 +0100

    Correction de l'affichage des évaluations quand on est pas connecté

--- www/view.php
+++ www/view.php
@@ -18,9 +18,10 @@ switch($media->media_type){
     default:
         $currentPage="noMedia";
 }
-
-$liked=Evaluation::getPrecise($_SESSION['user']->id_user,$_GET['id'],'like') instanceof Evaluation;
-$disliked=Evaluation::getPrecise($_SESSION['user']->id_user,$_GET['id'],'dislike') instanceof Evaluation;
-$favorited=Evaluation::getPrecise($_SESSION['user']->id_user,$_GET['id'],'favorite') instanceof Evaluation;
+if(isset($_SESSION['user'])){$id_user=$_SESSION['user']->id_user;} 
+else{$id_user=0;}
+$liked=(Evaluation::getPrecise($id_user,$_GET['id'],'like') instanceof Evaluation);
+$disliked=(Evaluation::getPrecise($id_user,$_GET['id'],'dislike') instanceof Evaluation);
+$favorited=(Evaluation::getPrecise($id_user,$_GET['id'],'favorite') instanceof Evaluation);
 
 require_once('./view/template.php');
