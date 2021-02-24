ce722227d5fb8fc4d48efddec7832cc1a39d6265
Pingumask <Pingumask@elanta.fr>
2021-02-24 16:49:44 +0100

    Optimisation de la récupération des évaluations

--- www/view.php
+++ www/view.php
@@ -18,10 +18,14 @@ switch($media->media_type){
     default:
         $currentPage="noMedia";
 }
-if(isset($_SESSION['user'])){$id_user=$_SESSION['user']->id_user;} 
-else{$id_user=0;}
-$liked=(Evaluation::getPrecise($id_user,$_GET['id'],'like') instanceof Evaluation);
-$disliked=(Evaluation::getPrecise($id_user,$_GET['id'],'dislike') instanceof Evaluation);
-$favorited=(Evaluation::getPrecise($id_user,$_GET['id'],'favorite') instanceof Evaluation);
+
+if(isset($_SESSION['user'])){
+    $liked=(Evaluation::getPrecise($id_user,$_GET['id'],'like') instanceof Evaluation);
+    $disliked=(Evaluation::getPrecise($id_user,$_GET['id'],'dislike') instanceof Evaluation);
+    $favorited=(Evaluation::getPrecise($id_user,$_GET['id'],'favorite') instanceof Evaluation);
+} 
+else{
+    $liked = $disliked = $favorited = false;
+}
 
 require_once('./view/template.php');
