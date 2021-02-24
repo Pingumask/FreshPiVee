499f6b522c29e629025fcdb69ed0dbe8fa147a07
Pingumask <Pingumask@elanta.fr>
2021-02-24 10:52:09 +0100

    Nouvelle likebar

--- www/view/components/evaluation_form.php
+++ www/view/components/evaluation_form.php
@@ -1,12 +1,11 @@
 <form id="evaluation" action="handle_evaluation.php" method="post">
-    <input type="submit" name="type" value="👍" <?php if($liked){echo 'class="active"';}?>>
-    <input type="submit" name="type" value="👎" <?php if($disliked){echo 'class="active"';}?>>
-    <input type="submit" name="type" value="🎀" <?php if($favorited){echo 'class="active"';}?>>
-    <input type="hidden" name="upload_id" value="<?php echo $media->id_upload?>"/>
-    <meter id="likeBar"
-       min="0" max="100"
-       high="0" optimum="100"
-       value="<?php echo $media->getLikePercentage();?>">
-       <?php echo $media->getLikePercentage();?>% likes
-    </meter>
+    <div>
+        <input type="submit" name="type" value="👍" <?php if($liked){echo 'class="active"';}?> />
+        <input type="submit" name="type" value="👎" <?php if($disliked){echo 'class="active"';}?> />
+        <input type="submit" name="type" value="🎀" <?php if($favorited){echo 'class="active"';}?> />
+        <input type="hidden" name="upload_id" value="<?php echo $media->id_upload?>" />
+    </div>
+    <div id="likeBarContainer" title="<?php echo $media->getLikePercentage();?>% likes">
+        <div id="likeBar" style="width:<?php echo $media->getLikePercentage();?>%"></div>
+    </div>
 </form>
